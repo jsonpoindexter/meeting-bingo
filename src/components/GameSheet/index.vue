adfb374ceea8e11f59081d06e212c343fbeca29e
Jason Poindexter <poindexter.json@gmail.com>
2019-10-25 10:08:38 -0700

    Prettier + Add items to boxLabels

--- src/components/GameSheet/index.vue
+++ src/components/GameSheet/index.vue
@@ -4,12 +4,12 @@
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import Box from "@/components/GameSheet/Box.vue";
+import { Component, Vue } from 'vue-property-decorator'
+import Box from '@/components/GameSheet/Box.vue'
 @Component({
   components: {
-    Box
-  }
+    Box,
+  },
 })
 export default class GameSheet extends Vue {
   boxLabels: string[] = [
@@ -22,15 +22,25 @@ export default class GameSheet extends Vue {
     "'Killing it'",
     "'---- can you go on mute'",
     "'Sorry, I was on mute'",
-    "FREE SPACE",
+    'FREE SPACE',
     "'--- here!' in the middle of a conversation",
-    "scrum is longer than 15min (free space)",
+    'scrum is longer than 15min (free space)',
     "'Launch Readiness'",
     "'What is the LOE on that?'",
     "'oh yeah, I forgot about that!'",
     "'Let's take that offline'",
-    "'Let's bring it up in war room'"
-  ];
+    "'Let's bring it up in war room'",
+    'loud typing w/ mic on',
+    "'I'm thinking...'",
+    "'Sorry, I missed that..'",
+    'screen sharing a DM',
+    "screen sharer's Download bar in the browser open",
+    'screen sharer has low laptop battery',
+    '"Analytics"',
+    '"SEO/OEP"',
+    "'Accessibility'",
+    "'Responsive'",
+  ]
 
   /**
    * Shuffles array in place. ES6 version
@@ -38,10 +48,10 @@ export default class GameSheet extends Vue {
    */
   shuffle(array: string[]) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[array[i], array[j]] = [array[j], array[i]]
     }
-    return array;
+    return array
   }
 }
 </script>
